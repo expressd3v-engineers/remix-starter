9b96dfe4e148c338cacec00f41b1f0ffb0b620c8
expressd3v <expressd3v@gmail.com>
2022-03-06 01:09:22 -0800

    set config

--- frontend-remix/remix.config.js
+++ frontend-remix/remix.config.js
@@ -2,10 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+    ignoredRouteFiles: [".*"],
+    appDirectory: "src",
+    assetsBuildDirectory: "public/build",
+    serverBuildPath: "build/index.js",
+    publicPath: "/build/",
+    serverBuildTarget: "node-cjs",
+    devServerPort: 8002,
+    devServerBroadcastDelay: 3000
 };
